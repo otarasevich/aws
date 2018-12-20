a2f5317c94e1da36885adb7509b8ba162175c6cf
Tarasevich oleg <tarasevich.oleg@gmail.com>
2018-12-20 14:53:15 +0300

    edit script

--- install-qrencode.sh
+++ install-qrencode.sh
@@ -1,6 +1,8 @@
 sudo apt-get update -y
 sudo apt-get install -y nginx git-core qrencode python-virtualenv
-sudo cp config/qrencode.conf /etc/nginx/sites-available/default
+
+git clone https://github.com/sl-2018-12/aws
+sudo cp aws/config/qrencode.conf /etc/nginx/sites-available/default
 
 git clone https://github.com/chubin/qrenco.de
 cd qrenco.de
@@ -10,8 +12,6 @@ ve/bin/pip install -r requirements.txt
 nohup ve/bin/python bin/srv.py >>log/qrencode.log 2>&1 &
 
 sudo /etc/init.d/nginx restart
-
-
 
 #sudo apt-get update -y                                                                                         |
 #sudo apt-get install -y nginx git-core qrencode python-virtualenv                                              |
